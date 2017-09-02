48d593b392c0459866c5c474f27cf53b19bd6447
ADondi <dondera_alexandru@yahoo.com>
2017-09-02 10:20:07 -0500

    Add Exercise2 folder

---
+++ Exercise2/ex1.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+/* This is a comment. */
+int main (int argc, char *argv[])
+{
+	int distance = 100;
+	
+
+	//this is also a comment
+	printf("You are %d miles away.\n", distance);
+	printf("I keep getting the error: swap file already exists!\n");
+	printf("Happend twice allready :(\n");
+	printf("Hopefully I'll get through the bootcamp!!!\n");
+	printf("Just familiarizing myself with C before\n");
+
+	return 0;
+}
